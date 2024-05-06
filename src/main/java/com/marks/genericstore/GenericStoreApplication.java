d49d3500ee5daa62151b18bf055e09877a7b2485
André Marks <andrem4rks@gmail.com>
2024-05-06 18:14:43 -0500

    first commit

---
+++ src/main/java/com/marks/genericstore/GenericStoreApplication.java
@@ -0,0 +1,13 @@
+package com.marks.genericstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericStoreApplication.class, args);
+	}
+
+}
